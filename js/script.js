7fbf4c6a27773f8c598b1ec98f6ce9e158299c58
LanaAuada <rm551143@fiap.com.br>
2023-03-07 11:49:01 -0300

    ambos add

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//este é um comentário de linha
+
+/**
+ * É um comentário
+ * de
+ * bloco
+ */
+
+console.log("oi gnt")
+console.error("buenos dias")
+console.warn("sera meo")
+console.info("acho q ss")
